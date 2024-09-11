814cba4e7a73cb4d461c93ba4de05d9718d8ba93
Gilberto <gilberto@teknik.mx>
2024-09-11 13:54:47 -0600

    studying jobs

---
+++ jobs/app/Jobs/Logger.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class Logger implements ShouldQueue
+{
+
+}
